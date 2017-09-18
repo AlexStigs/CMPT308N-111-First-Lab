3b23586c0021d239b0698e20067f4ed10ad4c193
AlexStigs <Alexander.Stigliano1@marist.edu>
2017-09-18 00:33:13 -0400

    The SQL extension for the Lab 3 Queries
    Ask for help about how to get  the final script to execute all of the commands in sequence from one command.

---
+++ CAP Query.sql
@@ -0,0 +1,37 @@
+﻿select month, totalUSD
+from Orders;
+
+
+Select name, city
+from Agents
+WHERE name = 'Smith';
+
+
+select pid, name, priceUSD
+from Products
+WHERE qty > 200010;
+
+
+Select name, city
+from Customers
+WHERE city = 'Duluth';
+
+Select name, city
+from Agents
+where city != 'New York' AND city != 'Duluth';
+
+Select *
+from Products
+where city != 'Dallas' AND city != 'Duluth' AND priceUSD >= 1.00;
+
+Select *
+from Orders
+where month = 'Mar' OR month = 'May';
+
+Select *
+from Orders
+where month = 'Feb' AND totalUSD >= 500;
+
+Select *
+from Orders
+where cid = 'c005';
